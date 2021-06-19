4284639bc903a8219528f1770a4661e3a70683b2
thezzisu <thezzisu@gmail.com>
2021-06-19 22:02:18 +0800

    feat: add sponser

---
+++ src/components/Beg.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="card">
+    <div class="card-header"><i>步骤3（可选）: 打赏开发者</i></div>
+    <div class="flex justify-center">
+      <img src="https://res.zisu.dev/defined/pay" class="max-h-96" />
+    </div>
+  </div>
+</template>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -7,6 +7,8 @@
     <z-choose />
     <hr class="mt-4" />
     <z-download />
+    <hr class="mt-4" />
+    <z-beg />
   </div>
 </template>
 
@@ -15,9 +17,10 @@ import ZDescription from '@/components/Description.vue'
 import ZFilter from '@/components/Filter.vue'
 import ZChoose from '@/components/Choose.vue'
 import ZDownload from '@/components/Download.vue'
+import ZBeg from '@/components/Beg.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  components: { ZFilter, ZChoose, ZDownload, ZDescription }
+  components: { ZFilter, ZChoose, ZDownload, ZDescription, ZBeg }
 })
 </script>
