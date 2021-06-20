ccf19cb5b5413a972ae619c97671c029a81132da
Zhang Zisu <thezzisu@gmail.com>
2021-06-20 00:18:31 +0000

    feat: add version display

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,9 @@
 <template>
   <header class="app-header">
-    <router-link to="/" class="px-4 font-bold">高考志愿填报助手</router-link>
+    <router-link to="/" class="px-4 font-bold">
+      高考志愿填报助手
+      <span class="text-xs">v{{ version }}</span>
+    </router-link>
     <router-link to="/help" class="px-4">使用说明</router-link>
     <router-link to="/sponsor" class="px-4">赞助</router-link>
     <app-spacer />
@@ -20,6 +23,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import AppSpacer from '@/components/AppSpacer.vue'
+import { version } from '@/utils/pkg'
 import { mdiGithub } from '@mdi/js'
 import Icon from '@/components/Icon.vue'
 import ZisuIcon from '@/assets/logo.svg'
@@ -28,6 +32,7 @@ export default defineComponent({
   components: { AppSpacer, Icon },
   setup() {
     return {
+      version,
       mdiGithub,
       ZisuIcon
     }
