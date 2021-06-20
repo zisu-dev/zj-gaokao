740b4a1094a201488bffd811841a80f4a3b658a3
Zhang Zisu <thezzisu@gmail.com>
2021-06-20 00:14:41 +0000

    fix: consist header format

--- src/views/Sponsor.vue
+++ src/views/Sponsor.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card">
-    <div class="card-header"><i>打赏开发者</i></div>
+    <div class="card-header">打赏开发者</div>
     <div class="flex justify-center">
       <img src="https://res.zisu.dev/defined/pay" class="max-h-96" />
     </div>
